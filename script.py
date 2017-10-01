5f0582a44870513a553d940d71388837b98a75bd
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-10-01 19:24:15 -0400

    Add todo display function

--- script.py
+++ script.py
@@ -7,9 +7,15 @@ def create_table():
 	query = "CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY, name TEXT, status BOOLEAN)"
 	cursor.execute(query)
 
-def insert_values(value):
+def insert_todo(value):
 	query = "INSERT INTO todos VALUES (NULL, ?, 0)"
 	cursor.execute(query, (value,))
+
+def display_todos():
+	query = "SELECT * FROM todos"
+	result = cursor.execute(query)
+	for row in result:
+		print(row)
 
 def save_and_close_db():
 	connection.commit() # Save changes to the database
@@ -17,8 +23,9 @@ def save_and_close_db():
 
 def main():
 	create_table()
-	user_input = input("Write in the todo name: ")
-	insert_values(user_input)
+	# user_input = input("Write in the todo name: ")
+	# insert_todos(user_input)
+	display_todos()
 	save_and_close_db()
 
 if __name__ == '__main__':
